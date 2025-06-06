9e36981c0eff21b1a9decd3b796b1bb5522085de
sarvar2511 <ssaydaliev130@gmail.com>
2025-06-06 20:34:44 +0900

    feat: Task A

--- train.js
+++ train.js
@@ -1,4 +1,21 @@
 console.log("TRAIN AREA");
+/*TASK A
+
+Harf sifatida kiritilgan birinchi parametr, 
+kiritilgan ikkinchi parametr tarkibida nechta ekanligini qaytaruvchi
+Funktsiya tuzing
+
+Masalan: countLetter("e", "engineer")
+'engineer' so'zi tarkibida 'e' harfi 3 marotaba takrorlanganligi uchun
+3 sonini qaytaradi
+*/
+
+const countLetter = (a, soz) =>
+  soz.split("").filter((harf) => harf === a).length;
+
+const natija = countLetter("a", "Sarvarbek");
+console.log(natija);
+
 //21. NodeJS event loop va Callback functionlarni o'rganamiz
 const list = [
   "Be a good student", //0-20
@@ -12,7 +29,7 @@ const list = [
 {
   /* 
   function maslahatByJack(a, callback) {
-    if (typeof a !== "number") callback("insert a number", null);
+    if   (typeof a !== "number") callback("insert a number", null);
     //callback("ERROR nomi", Biz qaytarmoqchi bolgan data);
     else if (a <= 20) callback(null, list[0]);
     else if (a > 20 && a <= 30) callback(null, list[1]);
@@ -43,23 +60,23 @@ single thread degani shu yerda namoyon
 }
 //22. Asychroous function
 {
-  async function maslahatByJack(a) {
-    if (typeof a !== "number") throw new Error("insert a number");
-    else if (a <= 20) return list[0];
-    else if (a > 20 && a <= 30) return list[1];
-    else if (a > 30 && a <= 40) return list[2];
-    else if (a > 40 && a <= 50) return list[3];
-    else if (a > 50 && a <= 60) return list[4];
-    else {
-      return new Promise((resolve, reject) => {
-        setInterval(() => {
-          resolve(list[5]);
-        }, 2000); // promise da set intervalni bir marta log qiladi. Agarrda callback functionda bolsa qayta qayta isga tushadi
-      });
-    } /*Promise function ni ishlatsak, SetTimeout ni ishlatsak boladi, bunda await bilan call qilinganda
-avval promise call qilingan function ishga tushadi, qolgan functionlar kutib turadi  
-  */
-  }
+  //   async function maslahatByJack(a) {
+  //     if (typeof a !== "number") throw new Error("insert a number");
+  //     else if (a <= 20) return list[0];
+  //     else if (a > 20 && a <= 30) return list[1];
+  //     else if (a > 30 && a <= 40) return list[2];
+  //     else if (a > 40 && a <= 50) return list[3];
+  //     else if (a > 50 && a <= 60) return list[4];
+  //     else {
+  //       return new Promise((resolve, reject) => {
+  //         setInterval(() => {
+  //           resolve(list[5]);
+  //         }, 2000); // promise da set intervalni bir marta log qiladi. Agarrda callback functionda bolsa qayta qayta isga tushadi
+  //       });
+  //     } /*Promise function ni ishlatsak, SetTimeout ni ishlatsak boladi, bunda await bilan call qilinganda
+  // avval promise call qilingan function ishga tushadi, qolgan functionlar kutib turadi
+  //   */
+  //   }
   //calling by then().catch()
   /*
 synchronous functionlar ishga tushganidan keyin asychronous functionlar ishga tushishni boshlaydi
@@ -74,7 +91,6 @@ shunign uchun ham asychronous funtion single hread ni band qilmaydi
   //     console.log("ERROR:", err);
   //   });
   // console.log("Passed here 1");
-
   /*Avval [1]ni keyin [2]ni... besin desak bir async yasab olamiz, kak maydoncha vazifasini bajaradi */
   //async/await
   // async function run() {
