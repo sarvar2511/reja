e041776a39d25392b838a420e472e38435820d29
sarvar2511 <ssaydaliev130@gmail.com>
2025-06-08 14:26:42 +0900

    fix: server.js ni yaxshiroq tushunish

--- server.js
+++ server.js
@@ -1,49 +1,101 @@
 console.log("Web Serverni boshlash");
-// const express = require("express");
-// const app = express();
-// const http = require("http");
-// //express dan app objectini yuboradi, va bu orqali express web server quramiz: 4 bosqichda:
+/*const express = require("express");
+//express package node.js da backend serverni qurishda yodam beradi
+const app = express();
+//expressning app objectidan backend serverni qurishda foydalanamiz
+const http = require("http");
+//http core package. bu serverni ishlatish uchun hizmat qiladigan object
+const fs = require("fs");
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
+}); //fs dan fodalanib user.json malumotimizni oqiydi/ suniy databaseni oqidi.
 
-// //1.Kirish code
-// // expreessga kirib kelayotgan  malumotlarga bogliq kodlar yoziladi
-// app.use(express.static("public")); //=> Bu har qanday browserdan kirib kelayotgan zaproslar uchun public folder ochiq degani.
-// //  Public folderni userlaga ochib beramiz. css styling va imagelar shuni ichida boaldi
-// app.use(express.json()); //bu kirib kelayotgan .json formatdagi malumotni object holatiga ogirib beadi
-// app.use(express.urlencoded({ extended: true })); // HTML form dan traditional request qilish elementi.
-// //  formdan biror nasa post qilsak bu narsa serverga kiritib beradi=>express qabul qiladi
+/*
+.ejs - backenddan kelyotgan maumotlarni joylashtirish uchun til/tool 
+Pattern (qolip) - ARCHITECTURE & DESIGN(Middlaware)
+ARCHITECTURE patern - Backendni 90%ni qurib beradiga qolip
+MVC => client=>CONTROLLER=>Model=>Database=>Model=>CONTROLLER=>View(.ejs)=>CONTROLLER=>client
 
-// //2. Session codes
+DESIGN(Middlaware)- pattern - malum bir oddiy ishlarni qiladigan qolip
+Kirish kodlari misol bola oladi
 
-// //3. Views codes
-// // Backend da HTML yasaymiz.
-// app.set("views", "views"); //views folderdan oqiydi
-// app.set("view engine", "ejs"); //ejs orqali backend ni ichida front-end ni yasaymiz
+(B)SSR & SPA 
+(B)SSR -Backend Site Server Rendering- Backend va frontend bir joyda boladi
+(Doim browser update bolishi kerak ozgarishlarni olish uchun)
 
-// //4.Routing codes
-// //  Routerlarga moljallangan.
-// app.get("/hello", function (req, res) {
-//   // localhost:3000/hello deb google gilsak ochib beradi
-//   res.end("<h1 >HELLO MIT by LEO </h1>"); //super string bilan style bersayam boladi htmlga
-// });
+SPA - Single Page Application- Backend va frontend alohida boladi.
+( browser update bolishi kerak emas ozgarishlarni olish uchun)
 
-// app.get("/gift", function (req, res) {
-//   res.end("<h1 > Siz sovgalar bolimidasiz </h1>"); // localhost:3000/gift deb google gilsak ochib beradi
-// });
 
-// const server = http.createServer(app);
-// let PORT = 3000;
-// server.listen(PORT, function () {
-//   console.log(`The server is running succesfully on port: ${PORT}`);
-// }); // serverga yukladik
+API REQUESTS:
+    1) TYPE       => Traditional api | Rest api | GraphQL api
+    2) METHOD     => GET | POST
+    3) STRUCTURE  => header | body
 
-// express framework orqali web server ni qurib oldik
-/* 
-1. BSSR=> backendda front end ni render qilib olish/ qurib olish va browserga yuborish
-2. react da quramiz(keyinroq organamiz)
+    (B)SSR da traditional api va rest api lar ishlatiladi
+    Rest API ozi SPA lar uchun ishlab chiqilgan
 
-loyihani boshqa qurulmada ham korinishi uchun guthub dan foydalanamiz
-git branch=> develop => da ishlab keyin masterga merge qilinadi.
-git branch=> master da qilamiz hozircha
+// GET- data basedan malumot olish uchun ishlaydi
+//POST esa malumotni ozi bn olib keladi va database usha malumotni yozadi
+
+
+  Backend server bir marta quriladi boldi, requestlar API larga kealdi
+ */
+/*//1.Kirish code
+app.use(express.static("public")); //Publicni ochib beradi=>
+//Bu mantiq backenddagi turgan malum bir fayl(public)ni frontendga ochib beradi.
+// Boshqa fayllar frontendga yopiq boladi
+app.use(express.json()); //REST-API ga kirish imkonini beradi=>
+// frontend kelyotgan json fayl datalarini objectga aylantirib beradi
+//json formatdagi datani serverga kiritib beradi
+app.use(express.urlencoded({ extended: true })); //Traditional API ni qabul qilish uchun hizmat qiladi=>
+//nested objectlar kelsa qabul qil degan kod
+
+//2. Session codes => Authentication=>Log in/Sing up jarayonlari
+
+//3. Views codes
+app.set("views", "views");
+//ejs ni qaysi fayldan izlay degan code
+app.set("view engine", "ejs");
+//ejs ni ishlatish uchun xizmat qiladigan code
+
+// //4.Routing codes => API lar
+console.log("Passed A");
+
+//1-API
+app.post("/create-item", (req, res) => {
+  console.log("Step-2: Backendga keldim");
+  console.log("Step-2: Frontenddan qabul qilgan malumotlarim");
+  console.log(req.body);
+  console.log("Step-4: Backenddan chiqib Frontedga ketyapman");
+  //post sorovlarimiz doim API larimizni body qismidan keladi
+  res.json({ test: "succes" });
+});
+//2-API
+app.get("/", function (req, res) {
+  //Bu function qachonki get method request amalga oshirilganda ishga tushadi
+  console.log("BU API DOIM ISHGA TUSHADI");
+  res.render("harid");
+  // render - ejs formatdagi malumotni frontendga uzatish
+});
+//API-3
+app.get("/author", (req, res) => {
+  res.render("author", { user: user });
+  //author.ejs ga usedan kelgan malumotni kiritish
+});
+console.log("Passed B");
+
+const server = http.createServer(app);
+//http objectini .createServer methodiga argument sifatida app ni pass qilsak bizga backend serverni qurib beradi
+let PORT = 3000;
+//serverni olib listen qiladigan bolsak server port larimiz ishga tushayapti
+server.listen(PORT, function () {
+  console.log(`The server is running succesfully on port: ${PORT}`);
+});
 */
 
 const express = require("express");
@@ -59,33 +111,28 @@ fs.readFile("database/user.json", "utf8", (err, data) => {
   }
 });
 //1.Kirish code
-app.use(express.static("public")); //MiddleWare Desing Pattern => Public folder tashqi olamga ochiqlaymiz
-app.use(express.json()); //MiddleWare Desing Pattern => Rest api hizmati
-app.use(express.urlencoded({ extended: true })); //MiddleWare Desing Pattern =>Traditional api hizmati
-// shu kod borligi uchun ejs dagi actionni qabul qiladi
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //2. Session codes
 //3. Views codes
 app.set("views", "views");
-app.set("view engine", "ejs"); // ejs backendda front end ni qurib berayapti
+app.set("view engine", "ejs");
 
 //4.Routing codes
 app.post("/create-item", (req, res) => {
-  // console.log(req.body); //bodyda kormoqchiman
-  //   console.log(req);
+  // console.log(req.body);
   res.json({ test: "succes" }); // va json shaklida malumotni qabul qilmoqchiman
 });
 app.get("/", function (req, res) {
-  res.render("harid"); // renderga fayl nomi yoziladi. views 2-qadamda korsatib beramiz.
-  //back end da front end ishga tushayapti
+  res.render("harid");
 });
 app.get("/author", (req, res) => {
-  res.render("author", { user: user }); //serverda link belgilaymiz
+  res.render("author", { user: user });
 });
 const server = http.createServer(app);
 let PORT = 3000;
 server.listen(PORT, function () {
   console.log(`The server is running succesfully on port: ${PORT}`);
 });
-// GET- data basedan malumot olish uchun ishlaydi
-//POST esa malumotni ozi bn olib keladi va data basge usha malumotni yozadi
--- train.js
+++ train.js
@@ -10,15 +10,14 @@ Masalan: countLetter("e", "engineer")
 3 sonini qaytaradi
 */
 
-function countLetter(harf, soz) {
-  const result = soz.split("");
-  const lastResult = result.filter((i) => i === harf);
-  console.log(
-    `${soz} ning ichida ${harf} dan ${lastResult.length}ta miqdorda bor`
-  );
-}
-
-countLetter("a", "Sarvarbek");
+// function countLetter(harf, soz) {
+//   const result = soz.split("");
+//   const lastResult = result.filter((i) => i === harf);
+//   console.log(
+//     `${soz} ning ichida ${harf} dan ${lastResult.length}ta miqdorda bor`
+//   );
+// }
+// countLetter("a", "Sarvarbek");
 
 //21. NodeJS event loop va Callback functionlarni o'rganamiz
 const list = [
@@ -31,79 +30,86 @@ const list = [
 ];
 //Callback function
 {
-  /* 
-  function maslahatByJack(a, callback) {
-    if   (typeof a !== "number") callback("insert a number", null);
-    //callback("ERROR nomi", Biz qaytarmoqchi bolgan data);
-    else if (a <= 20) callback(null, list[0]);
-    else if (a > 20 && a <= 30) callback(null, list[1]);
-    else if (a > 30 && a <= 40) callback(null, list[2]);
-    else if (a > 40 && a <= 50) callback(null, list[3]);
-    else if (a > 50 && a <= 60) callback(null, list[4]);
-    else {
-      setTimeout(function () {
-        callback(null, list[5]);
-      }, 3000);
-    }
-  }
-  /* Shu callback functonni foydali tomini shu yerda, 
-setTimeOut qatnashgan awerni ishga tushidi lekin natijasini kutib otirmasdan
-pastidagi boshqa functonni shga tushirdi, vaqti kelganidan song keyin TipByJack ishga tushdi
-single thread degani shu yerda namoyon
+  //   function maslahatByJack(a, callback) {
+  //     if (typeof a !== "number") callback("insert a number", null);
+  //     //callback("ERROR nomi", Biz qaytarmoqchi bolgan data);
+  //     else if (a <= 20) {
+  //       setTimeout(function () {
+  //         callback(null, list[0]);
+  //       }, 2000);
+  //     } else if (a > 20 && a <= 30) callback(null, list[1]);
+  //     else if (a > 30 && a <= 40) callback(null, list[2]);
+  //     else if (a > 40 && a <= 50) callback(null, list[3]);
+  //     else if (a > 50 && a <= 60) callback(null, list[4]);
+  //     else {
+  //       setTimeout(function () {
+  //         callback(null, list[5]);
+  //       }, 5000);
+  //     }
+  //   }
+  //   /* Shu callback functonni foydali tomini shu yerda,
+  // setTimeOut qatnashgan awerni ishga tushidi lekin natijasini kutib otirmasdan
+  // pastidagi boshqa functonni shga tushirdi, vaqti kelganidan song keyin TipByJack ishga tushdi
+  // single thread degani shu yerda namoyon*/
+  //   console.log("Passed here 0");
+  //   maslahatByJack(70, (err, data) => {
+  //     if (err) console.log("ERROR:", err);
+  //     else {
+  //       console.log("TipByJack:", data);
+  //     }
+  //   });
+  //   maslahatByJack(18, (err, data) => {
+  //     if (err) console.log("ERROR:", err);
+  //     else {
+  //       console.log("TipByJack:", data);
+  //     }
+  //   });
+  //   console.log("Passed here 1");
+}
 
-  console.log("Passed here 0");
-  maslahatByJack(70, (err, data) => {
-    if (err) console.log("ERROR:", err);
-    else {
-      console.log("TipByJack:", data);
-    }
-  });
-  console.log("Passed here 1");
-}
-*/
-}
 //22. Asychroous function
 {
-  //   async function maslahatByJack(a) {
-  //     if (typeof a !== "number") throw new Error("insert a number");
-  //     else if (a <= 20) return list[0];
-  //     else if (a > 20 && a <= 30) return list[1];
-  //     else if (a > 30 && a <= 40) return list[2];
-  //     else if (a > 40 && a <= 50) return list[3];
-  //     else if (a > 50 && a <= 60) return list[4];
-  //     else {
-  //       return new Promise((resolve, reject) => {
-  //         setInterval(() => {
-  //           resolve(list[5]);
-  //         }, 2000); // promise da set intervalni bir marta log qiladi. Agarrda callback functionda bolsa qayta qayta isga tushadi
-  //       });
-  //     } /*Promise function ni ishlatsak, SetTimeout ni ishlatsak boladi, bunda await bilan call qilinganda
-  // avval promise call qilingan function ishga tushadi, qolgan functionlar kutib turadi
-  //   */
-  //   }
+  async function maslahatByJack(a) {
+    if (typeof a !== "number") throw new Error("insert a number");
+    else if (a <= 20) return list[0];
+    else if (a > 20 && a <= 30) return list[1];
+    else if (a > 30 && a <= 40) return list[2];
+    else if (a > 40 && a <= 50) return list[3];
+    else if (a > 50 && a <= 60) return list[4];
+    else {
+      return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve(list[5]);
+        }, 2000); // promise da set intervalni bir marta log qiladi. Agarrda callback functionda bolsa qayta qayta isga tushadi
+      });
+    } /*Promise function ni ishlatsak, SetTimeout ni ishlatsak boladi, bunda await bilan call qilinganda
+  avval promise call qilingan function ishga tushadi, qolgan functionlar kutib turadi
+    */
+  }
   //calling by then().catch()
   /*
 synchronous functionlar ishga tushganidan keyin asychronous functionlar ishga tushishni boshlaydi
-shunign uchun ham asychronous funtion single hread ni band qilmaydi
+shunign uchun ham asychronous funtion single thread ni band qilmaydi
 */
-  // console.log("Passed here 0");
-  // maslahatByJack(25)
-  //   .then((data) => {
-  //     console.log("Jack says:", data);
-  //   })
-  //   .catch((err) => {
-  //     console.log("ERROR:", err);
-  //   });
-  // console.log("Passed here 1");
+  console.log("Passed here 0");
+  maslahatByJack(25)
+    .then((data) => {
+      console.log("Jack says:", data);
+    })
+    .catch((err) => {
+      console.log("ERROR:", err);
+    });
+  console.log("Passed here 1");
   /*Avval [1]ni keyin [2]ni... besin desak bir async yasab olamiz, kak maydoncha vazifasini bajaradi */
   //async/await
-  // async function run() {
-  //   let javob = await maslahatByJack(20);
-  //   console.log(javob);
-  //   javob = await maslahatByJack(65);
-  //   console.log(javob);
-  //   javob = await maslahatByJack(41);
-  //   console.log(javob);
-  // }
-  // run();
+
+  async function run() {
+    let javob = await maslahatByJack(20);
+    console.log(javob);
+    javob = await maslahatByJack(65);
+    console.log(javob);
+    javob = await maslahatByJack(41);
+    console.log(javob);
+  }
+  run();
 }
