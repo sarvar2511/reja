ca96463969c9cbabe6987dbf5a0776b2a2d31fa9
sarvar2511 <ssaydaliev130@gmail.com>
2025-06-13 20:32:43 +0900

    feat: tasl-c

--- train.js
+++ train.js
@@ -1,4 +1,68 @@
 console.log("TRAIN AREA");
+/*
+TASK-C
+
+Shop nomli class tuzing, va bu class 3 xill parametr qabul qilsin.
+Hamda classning quyidagdek 3'ta metodi bo'lsin:
+
+1) qoldiq
+2) sotish
+3) qabul
+
+Har bir metod ishga tushgan vaqtda log qilinsin
+
+MASALAN:
+const shop = new Shop(4, 5, 2)
+
+shop.qoldiq();
+natija qaytishi kerak: Hozir 20: 40'da 4'ta non, 5'ta lag'mon va 2'ta cola mavjud
+
+shop.sotish("non", 3); & shop.qabul("cola", 4); & shop.qoldiq();
+Natija qaytishi kerak: Hozir 20:50da 1ta non, 5ta lag'mon va 6ta cola mavjud!
+*/
+const moment = require("moment");
+class Shop {
+  constructor(non, lagmon, cola) {
+    this.non = non;
+    this.lagmon = lagmon;
+    this.cola = cola;
+  }
+
+  getTime() {
+    return moment().format("HH:mm");
+  }
+
+  qoldiq() {
+    console.log(
+      `Hozir ${this.getTime()}da ${this.non}ta non, ${
+        this.lagmon
+      }ta lag'mon va ${this.cola}ta cola mavjud`
+    );
+  }
+
+  sotish(tovar, soni) {
+    if (this[tovar] !== undefined) {
+      this[tovar] -= soni;
+    } else {
+      console.log(`Bunday mahsulot yo‘q: ${tovar}`);
+    }
+  }
+
+  qabul(tovar, soni) {
+    if (this[tovar] !== undefined) {
+      this[tovar] += soni;
+    } else {
+      console.log(`Bunday mahsulot yo‘q: ${tovar}`);
+    }
+  }
+}
+const shop = new Shop(4, 5, 2);
+
+shop.qoldiq();
+shop.sotish("non", 3);
+shop.qabul("cola", 4);
+shop.qoldiq();
+
 /*B-TASK
 
 Shunday function tuzing, u 1ta string parametrga ega bolsin, hamda osha stringda qatnashgan raqamlarni sonini bizga return qilsin.
